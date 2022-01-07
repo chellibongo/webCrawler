b5cd5f19b957097dea24e8d9f9400d1be2b49bad
Chris <ce8958@gmail.com>
2022-01-07 18:52:27 +0000

    Creating new webCrawler repo

---
+++ src/fileWriter.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class fileWriter {
+    
+    //create file
+    public void createFile(File file){
+
+    }
+
+    //write text to file
+    public void writeFile(String text) {
+
+    }
+
+}
